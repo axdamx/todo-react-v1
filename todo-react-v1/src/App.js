1f247ddbc643ee9a2322191aea666395d40c8f1c
axdamx <axdamx@gmail.com>
2020-10-15 20:09:17 +0800

    project init

---
+++ todo-react-v1/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1> React CRUD with Hooks </h1>
+      <div className="row">
+        <div className="five columns">
+          <h2> Add User </h2>
+        </div>
+        <div className="seven columns">
+          <h2> View User </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
